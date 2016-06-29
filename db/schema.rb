c5112a169f86ddecaba4e08e492147becd2a5433
Riley Scheid <riley.kenyon@gmail.com>
2016-06-29 14:00:29 -0700

    Change schema to allow for dump restore

--- db/schema.rb
+++ db/schema.rb
@@ -16,49 +16,6 @@ ActiveRecord::Schema.define(version: 20160605183455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.string   "body"
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "flags", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "flaggable_id"
-    t.string   "flaggable_type"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "flags", ["flaggable_type", "flaggable_id"], name: "index_flags_on_flaggable_type_and_flaggable_id", using: :btree
-  add_index "flags", ["user_id"], name: "index_flags_on_user_id", using: :btree
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "user_id"
-    t.integer  "school_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "posts", ["school_id"], name: "index_posts_on_school_id", using: :btree
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
-
-  create_table "schools", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "address"
-    t.decimal  "lat"
-    t.decimal  "lng"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "service_providers", force: :cascade do |t|
     t.string   "name",               null: false
     t.string   "address"
@@ -74,18 +31,4 @@ ActiveRecord::Schema.define(version: 20160605183455) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "email"
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "flags", "users"
-  add_foreign_key "posts", "schools"
-  add_foreign_key "posts", "users"
 end
